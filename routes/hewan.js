10319b58bd47166b89427a0851ffc44678064f7e
roy <111065538+kaizeenn@users.noreply.github.com>
2025-05-11 21:43:38 +0700

    menampilkan gambar dan audio hewan dan buah, meperbaiki tampilan maseter

--- routes/hewan.js
+++ routes/hewan.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const db = require('../config/database');
+const db = require("../config/database");
 
-router.get('/', function(req, res, next){
-    db.query(`select * from object`, (err, rows) => {
-        if (err) return res.status(500).json({ error: err.message });
-        console.log(rows)
-        res.render('hewan/index', { data: rows});
-    })
-})
+router.get("/", function (req, res, next) {
+  db.query(`select * from object where id_cat = 1`, (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    console.log(rows);
+    res.render("hewan/index", { data: rows });
+  });
+});
 
 module.exports = router;
